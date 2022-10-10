59940b1257af5e2d7c178e22b836494ec55115c0
Mingu Kim <mkim245@gmail.com>
2022-10-10 12:41:35 -0400

    roll dice without including selected dice

--- src/App.js
+++ src/App.js
@@ -6,27 +6,37 @@ import { nanoid } from "nanoid"
 function App() {
   const [dice, setDice] = React.useState(allNewDice());
 
+  function generateNewDie() { //helper function
+    return {
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.floor(Math.random() * 6) + 1,
-        isHeld: false,
-        id: nanoid()
-      });
+      newDice.push(generateNewDie())
     }
     return newDice;
   }
 
   function rollDice() {
-    setDice(allNewDice())
+    // setDice(allNewDice())
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld
+        ? die
+        : generateNewDie()
+    }))
+
   }
 
   function holdDice(id) {
     setDice(oldDice => oldDice.map(die => {
-      return die.id === id 
-      ? {...die, isHeld: !die.isHeld} 
-      : die
+      return die.id === id
+        ? { ...die, isHeld: !die.isHeld }
+        : die
     }))
   }
 
@@ -41,6 +51,8 @@ function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {dieElements}
       </div>
