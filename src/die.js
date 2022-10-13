50f839c3e35880e5bc004b8c3aefd858fb2abfbd
Mingu Kim <mkim245@gmail.com>
2022-10-12 23:15:57 -0400

    change number to alphabet

--- src/die.js
+++ src/die.js
@@ -4,12 +4,40 @@ export default function Die(props) {
   const styles = {
     backgroundColor: props.isHeld ? "#59E391" : "white"
   }
+
+  function numberToDot(number) {
+    if (number === 1) {
+      return "A"
+    }
+    if (number === 2) {
+      return "B"
+    }
+    if (number === 3) {
+      return "C"
+    }
+    if (number === 4) {
+      return "D"
+    }
+    if (number === 5) {
+      return "E"
+    }
+    if (number === 6) {
+      return "F"
+    }
+  }
+
+
   return (
-    <div className="die-face" 
-    style={styles}
-    onClick={props.holdDice}
+    <div className="die-face"
+      style={styles}
+      onClick={props.holdDice}
     >
-      <h2 className="die-num">{props.value}</h2>
+      {/* <h2 className="die-num">{props.value}</h2> */}
+      <span
+        className={props.value === 1 ? "dot" : "die-num"}
+      >
+        {numberToDot(props.value)}
+      </span>
     </div>
   )
 }
